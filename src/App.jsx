75887f2f6f22340033e5fbaf9a22b49c82c32370
Adela Olivera <157389359+adela0907@users.noreply.github.com>
2024-07-15 10:46:25 -0300

    Add files via upload
    falta conectar API para deployar

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import GlobalStyles from "./components/globalStyles/GlobalStyles"
+import AppRoutes from "./routes/Routes"
+
+const App = () => {
+
+  return (
+    <>
+      <GlobalStyles />
+    
+      <AppRoutes />
+    </>
+  )
+}
+
+export default App
